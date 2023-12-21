a5c0680954ac0c4ec46a51083054481345e3702d
Gabe <gabriel.velasquez0116@gmail.com>
2023-12-20 16:57:53 -0800

    added randopm quote gen to profile page

--- app/(tabs)/Profile.tsx
+++ app/(tabs)/Profile.tsx
@@ -1,11 +1,29 @@
 import { StyleSheet } from 'react-native';
+import {useState,useEffect} from 'react';
 import { Text, View } from '../../components/Themed';
 
 export default function Profile() {
+  const api_url = 'https://zenquotes.io/api/quotes/';
+   const [quote, setQuote] = useState('');
+   const [author,setAuthor]=useState('')
+
+   useEffect(() => {
+       getapi(api_url);
+   }, []);
+
+   async function getapi(url:string) {
+       const response = await fetch(url);
+       const data = await response.json();
+       setQuote(data[0].q);
+       setAuthor(data[0].a);
+   }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>W</Text>
+      <Text style={styles.title}>Hello!</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text style={styles.text}>{quote}</Text>
+      <Text style={styles.text}>- {author}</Text>
       {/* <EditScreenInfo path="app/(tabs)/two.tsx" /> */}
     </View>
   );
@@ -26,4 +44,7 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  text:{
+    color:'black'
+    }
 });
