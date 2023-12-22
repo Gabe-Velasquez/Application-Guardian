f723e8dde581b482fb31ebf42888be6fa6929f91
Gabe <gabriel.velasquez0116@gmail.com>
2023-12-22 13:28:09 -0800

    adding quotes bank to be randomly called and displayed to users on home page

---
+++ constants/quotes.ts
@@ -0,0 +1,112 @@
+interface Quote {
+    quote: string;
+    author: string;
+  }
+  
+  const quotes: Quote[] = [
+    {
+      quote: `Success is not final, failure is not fatal. It is the courage to continue that counts.`,
+      author: `Winston Churchill`
+    },
+    {
+        quote: `You may encounter many defeats, but you must not be defeated. In fact, it may be necessary to encounter the defeats, so you can know who you are, what you can rise from, how you can still come out of it.`,
+        author: `Maya Angelou`
+    },
+    {
+      quote: `However difficult life may seem, there is always something you can do and succeed at.`,
+      author: `Stephen Hawking`
+    },
+    {
+        quote: `The brick walls are there for a reason. The brick walls are not there to keep us out. The brick walls are there to show us how badly we want something.`,
+        author: `Randy Pausch`
+    },
+    {
+        quote: `Where there is a will, there is a way. If there is a chance in a million that you can do something, anything, to keep what you want from ending, do it. Pry the door open or, if need be, wedge your foot in that door and keep it open.`,
+        author: `Pauline Kael`
+    },
+    {
+        quote: `I am convinced that most people can achieve their dreams and beyond if they have the determination to keep trying.`,
+        author: `Howard Schultz`
+    },
+    {
+        quote: `There will be obstacles. There will be doubters. There will be mistakes. But with hard work, there are no limits.`,
+        author: `Michael Phelps`
+    },
+    {
+        quote: `Experience is not what happens to a man. It is what a man does with what happens to him.`,
+        author: `Aldous Huxley`
+    },
+    {
+        quote: `Never give up, for that is just the place and time that the tide will turn.`,
+        author: `Harriet Beecher Stowe`
+    },
+    {
+        quote: `I have discovered in life that there are ways of getting almost anywhere you want to go if you really want to go.`,
+        author: `Langston Hughes`
+    },
+    {
+        quote: `Our greatest glory is not in never falling, but in rising every time we fall.`,
+        author: `Confucius`
+    },
+    {
+        quote: `Don't be pushed around by the fears in your mind. Be led by the dreams in your heart.`,
+        author: `Roy T. Bennett`
+    },
+    {
+        quote:``,
+        author:``
+    },
+    {
+        quote:``,
+        author:``
+    },
+    {
+        quote:``,
+        author:``
+    },
+    {
+        quote:``,
+        author:``
+    },
+    {
+        quote:``,
+        author:``
+    },
+    {
+        quote:``,
+        author:``
+    },
+    {
+        quote:``,
+        author:``
+    },
+    {
+        quote:``,
+        author:``
+    },
+    {
+        quote:``,
+        author:``
+    },
+    {
+        quote:``,
+        author:``
+    },
+    {
+        quote:``,
+        author:``
+    },
+    {
+        quote:``,
+        author:``
+    },
+    {
+        quote:``,
+        author:``
+    },
+    {
+        quote:``,
+        author:``
+    }
+  ];
+  
